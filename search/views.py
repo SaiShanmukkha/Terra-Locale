e9aaa0340909104f70976f4dd0c099b2744bb798
6hsenag9 <gganeshjobs1@gmail.com>
2024-03-28 13:44:27 -0500

    adding the completed search functionality

---
+++ search/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+# from products.models import Product
+from cart.models import Product,Category
+from django.db.models import Q
+from django.views import generic
+from typing import Any, Dict, Union, List
+
+# from elasticsearch_dsl import analyzer, tokenizer
+
+# autocomplete_analyzer = analyzer('autocomplete_analyzer',
+#             tokenizer=tokenizer('trigram', 'nGram', min_gram=1, max_gram=20),
+#             filter=['lowercase']
+#         )
+
+def do_search(request):
+    category= Category.objects.all()
+    products = Product.objects.filter(title__icontains=request.GET['q'])
+    return render(request, "cart/product_list.html", {"products": products})
+# Create your views here.
+from cart.models import (
+    Product,
+    OrderItem,
+    Address,
+    Payment,
+    Order,
+    Category,
+    MainCategory,
+    StripePayment,
+)
+
+class ProductSearchListView(generic.ListView):
+    template_name: str = 'cart/product_list.html'
+
+    def get_queryset(self):
+        qs = Product.objects.all()
+        cs=Category.objects.all()
+        
+        main_category=self.request.GET.get('mainCategory',None)
+        search = self.request.GET.get('search', None)
+        print("sarch keword",search)
+
+        
+        print("vaues",qs.filter(Q(slug__icontains=search)))
+        if not main_category:
+            if not search:
+                return qs
+            return qs.filter(
+                Q(title__icontains=search) |Q(secondary_categories__name__icontains=search)|
+                Q(primary_category__name__icontains=search)|Q(primary_category__in = cs.filter(Q(main_category__name__icontains=search)))#|
+               # Q( "multi_match",query=search,fields=['title', ], fuzziness='auto',))
+               ).distinct()
+        return qs.filter(primary_category__in = cs.filter(Q(main_category__name=main_category)))
+        
+        
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
+        context = super(ProductSearchListView, self).get_context_data(**kwargs)
+        context.update({"categories": Category.objects.values("name")})
+        context.update({"mainCategory":MainCategory.objects.all().order_by('name')})
+        return context
+
+__all__ = (
+    'ProductSearchListView',)
+
